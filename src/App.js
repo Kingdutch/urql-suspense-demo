7e1117bfbe025ea4fdd33aec8c787eb0e3894f35
Alexander Varwijk <alexander@getopensocial.com>
2020-12-10 13:40:05 +0100

    Create simple reproduction
    See https://github.com/FormidableLabs/reason-urql/issues/232

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from "react";
+import { createClient, Provider, useQuery } from 'urql';
 
-function App() {
+const client = createClient({
+  url: 'https://countries.trevorblades.com/',
+  suspense: true,
+});
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider value={client}>
+      <div className="App">
+        <h1>Urql Suspense demo</h1>
+        <Suspense fallback={<p>Loading...</p>}>
+          <SuspenseLoading />
+        </Suspense>
+      </div>
+    </Provider>
   );
+  
 }
 
-export default App;
+const UrqlQuery = `
+query {
+  country(code:"NL") {
+    name
+    emoji
+  }
+}
+`;
+
+function SuspenseLoading() {
+  const [result, ] = useQuery({ query: UrqlQuery });
+  return (
+    <div>
+      <h2>The flag of {result.data.country.name}</h2>
+      <p>{result.data.country.emoji}</p>
+    </div>
+  )
+}
